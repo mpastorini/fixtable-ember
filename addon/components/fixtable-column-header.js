42b33c30d8e062c2c59adf4abef845f355b7a53f
Mauro Hugo Pastorini <mauro.h.pastorini@gmail.com>
2017-03-02 15:05:03 -0300

    filters-th

--- addon/components/fixtable-column-header.js
+++ addon/components/fixtable-column-header.js
@@ -4,14 +4,31 @@ import layout from '../templates/components/fixtable-column-header';
 export default Ember.Component.extend({
   layout,
   tagName: 'th',
-
   column: null,
   sortBy: null,
   sortAscending: null,
-
+  classNameBindings: ['filterActive:active', 'select_head:select_all'],
+  select_head: Ember.computed('column', function(){
+    if(this.get('column.key') === 'select_all'){
+      return true;
+    }
+    return false;
+  }),
+  filterActive: false,
   actions: {
     sortColumn(columnKey) {
       this.sendAction('sortColumn', columnKey);
+    },
+    toggleFilter(){
+      this.attrs.closeAllFilters();
+      this.toggleProperty('filterActive');
+    },
+    selectAll() {
+      this.set('filter', true);
     }
+  },
+  didInsertElement(){
+    this._super(...arguments);
+    this.$().css('min-width', this.get('column.width'));
   }
 });
