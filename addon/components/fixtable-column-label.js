7a44c28fdbec3bfa6976dc1c78f936439071d56a
Mauro Hugo Pastorini <mauro.h.pastorini@gmail.com>
2017-03-02 15:05:28 -0300

    filter-th

--- addon/components/fixtable-column-label.js
+++ addon/components/fixtable-column-label.js
@@ -4,4 +4,12 @@ import layout from '../templates/components/fixtable-column-label';
 export default Ember.Component.extend({
   layout,
   tagName: 'span',
+  actions: {
+    toggleFilter(){
+      this.toggleProperty('filterActive');
+    },
+    selectAll(){
+    	this.attrs.applyManualFilter();
+    }
+  }
 });
